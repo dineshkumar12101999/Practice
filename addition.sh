ea9b1433ea8e77234ebcc8d12272bdfd723e12fe
dineshkumar12101999 <dineshkskumar7@gmail.com>
2024-10-24 21:06:06 +0530

    Addition program

---
+++ addition.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get the first number
+read -p "Enter the first number: " num1
+
+# Get the second number
+read -p "Enter the second number: " num2
+
+# Perform addition
+echo "the answer is"
+sum=$((num1 + num2))
